012aa22483b3e1b8c17923eeb45c76c6af85218d
LJZSR <jzs1996@sjtu.edu.cn>
2018-11-22 09:53:07 +0800

    Update vsearch4web.py

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, escape,session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from vsearch import search4letters
 from checker import check_logged_in
 from DBcm import UseDataBase, ConnectionError, CredentialsError, SQLError
+from threading import Thread
+from time import sleep
 
 app = Flask(__name__)
 
@@ -20,27 +22,28 @@ def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out.'
 
-def log_request(req: 'flask_request', res: str) -> None:
-    raise Exception('Something awful just happened.')
-    with UseDataBase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                  (phrase, letters, ip, browser_string, results)
-                  values 
-                  (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        sleep(15)
+        with UseDataBase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                   (phrase, letters, ip, browser_string, results)
+                   values
+                   (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res, ))
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are the results:'
     results = str(search4letters(phrase, letters))
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('Logging failed with this error:', str(err))
     return render_template('results.html',
